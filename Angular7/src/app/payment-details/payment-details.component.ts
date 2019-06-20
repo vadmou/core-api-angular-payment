a4e7bbfde5ad29b4435d495bc9d095ac16d6ec9b
vadmou <vadmou@gmail.com>
2019-06-20 09:42:38 +0300

    Add files via upload

---
+++ Angular7/src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-details',
+  styles: []
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
